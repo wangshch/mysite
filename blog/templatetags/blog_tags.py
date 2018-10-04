719147514e3c5578f7e9b03e7cc994076262947d
wangshch <wsc.19@163.com>
2018-10-04 15:36:43 +0800

    Initial commit

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from blog.models import Post, Category
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_recent_posts(num=5):
+    return Post.objects.all().order_by('-create_time')[:num]
+
+
+@register.simple_tag
+def archives():
+    return Post.objects.dates('create_time', 'month', order='DESC')
+
+
+@register.simple_tag
+def get_categories():
+    # 别忘了在顶部引入 Category 类
+    return Category.objects.all()
